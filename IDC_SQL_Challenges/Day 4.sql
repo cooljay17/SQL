3f6042c85636d76fe4bafaa4b09684c1ab269345
Jayanthi Manickam Sethu <coolj1784@gmail.com>
2025-11-06 10:48:43 +0530

    Imit offset

---
+++ IDC_SQL_Challenges/Day 4.sql
@@ -0,0 +1,22 @@
+--LIMIT and OFFSET
+--Challenge Question
+/*Find the 3rd to 7th highest patient satisfaction scores from the patients table, 
+showing patient_id, name, service, and satisfaction. Display only these 5 records.*/
+select patient_id, name, service, satisfaction
+from patients
+order by satisfaction desc,patient_id
+offset 2
+limit 5;
+
+--Practice Questions
+--1. Display the first 5 patients from the patients table.
+select * from patients limit 5;
+--2. Show patients 11-20 using OFFSET.
+select * from patients 
+order by patient_id
+offset 10
+limit 10;
+--3. Get the 10 most recent patient admissions based on arrival_date.
+select * from patients 
+order by arrival_date desc 
+limit 10;
